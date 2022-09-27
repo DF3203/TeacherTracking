aca5ac5782dd2d732cb20ed752a1258bd5afdcf9
Wildrake <astakhov.vlad@gmail.com>
2022-09-27 12:53:50 +0300

    cock

--- CourseWork/wwwroot/js/authorization.js
+++ CourseWork/wwwroot/js/authorization.js
@@ -1,79 +1,81 @@
 
-(function ($) {
-    "use strict";
+//(function ($) {
+//    "use strict";
 
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+//    /*==================================================================
+//    [ Validate ]*/
+//    var input = $('.validate-input .input100');
 
-    $('.validate-form').on('button',function(){
-        var check = true;
+//    $('.validate-form').on('button',function(){
+//        var check = true;
+//        for(var i=0; i<input.length; i++) {
+//            if(validate(input[i]) == false){
+//                showValidate(input[i]);
+//                check=false;
+//            }
+//        }
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
+//        return check;
+//    });
 
-        return check;
-    });
+//    $('.validate-form .input100').each(function(){
+//        $(this).focus(function(){
+//           hideValidate(this);
+//        });
+//    });
 
+//    function validate (input) {
+//        if($(input).attr('type') == 'text' || $(input).attr('name') == 'email') {
+//            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+//                return false;
+//            }
+//        }
+//        else {
+//            if($(input).val().trim() == ''){
+//                return false;
+//            }
+//        }
+//    }
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
+//    function showValidate(input) {
+//        var thisAlert = $(input).parent();
 
-    function validate (input) {
-        if($(input).attr('type') == 'text' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
+//        $(thisAlert).addClass('alert-validate');
+//    }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+//    function hideValidate(input) {
+//        var thisAlert = $(input).parent();
 
-        $(thisAlert).addClass('alert-validate');
-    }
+//        $(thisAlert).removeClass('alert-validate');
+//    }
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+//})(jQuery);
 
-        $(thisAlert).removeClass('alert-validate');
-    }
-    console.log(1);
-})(jQuery);
+//function checkUser()
+//{
+//    if (getCookie("user") !== undefined && getCookie("id_user") !== undefined)
+// window.location = "mainPage.html";
+//}
 
-function checkUser()
-{
-    if (getCookie("user") !== undefined && getCookie("id_user") !== undefined)
- window.location = "mainPage.html";
-}
+const button = document.querySelector(".login100-form-btn");
+
+button.addEventListener("click", onLoginClick());
 
 function onLoginClick()
 {
-//    let login = document.getElementsByName("username")[0];
-//    let password = document.getElementsByName("pass")[0];
+    let login = document.getElementsByName("email")[0];
+    let password = document.getElementsByName("pass")[0];
 
-//    let input = $('.validate-input .input100');
+    let input = $('.validate-input .input100');
 
+    if (login.value.toString().length === 0 || (password.value.toString().length === 0))
+    {
+       $($(input).parent()).addClass('alert-validate');
+       login.value = "";
+       password.value = "";
+       return;
+    }
 
-//    if (login.value.toString().length === 0 || (password.value.toString().length === 0))
-//    {
-//        $($(input).parent()).addClass('alert-validate');
-//        login.value = "";
-//        password.value = "";
-//        return;
-//    }
 
 //    let request_CheckLogin = new XMLHttpRequest();
 //    request_CheckLogin.onreadystatechange = function ()
