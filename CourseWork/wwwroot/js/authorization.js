72917842fefeb52b1f999dfb71b4ede81390850a
DF3203 <slvstgl90238@gmail.com>
2022-10-12 19:50:12 +0300

    Reworked JS

--- CourseWork/wwwroot/js/authorization.js
+++ CourseWork/wwwroot/js/authorization.js
@@ -1,4 +1,4 @@
-import * as Cookies from "./cookies.js";
+﻿import * as Cookies from "./cookies.js";
 if (Cookies.getCookie("user") !== undefined && Cookies.getCookie("id_user") !== undefined)
 window.location = "mainPage.cshtml";
 
@@ -10,12 +10,19 @@ const login = document.querySelector('input[name="login"]');
 const password = document.querySelector('input[name="pass"]');
 
 const onLoginInput = () => {
-    if (login.value.length < 6) login.parentNode.classList.add('alert-validate');
+    if (login.value.length < 6)
+    {
+        login.parentNode.setAttribute('data-validate', "Мінімум 6 символів");
+        login.parentNode.classList.add('alert-validate');
+    }
     else login.parentNode.classList.remove('alert-validate');
 }
 
 const onPasswordInput = () => {
-    if (password.value.length < 8) password.parentNode.classList.add('alert-validate');
+    if (password.value.length < 8) {
+        password.parentNode.setAttribute('data-validate', "Мінімум 8 символів");
+        password.parentNode.classList.add('alert-validate');
+    }
     else password.parentNode.classList.remove('alert-validate');
 }
 
@@ -26,5 +33,10 @@ password.addEventListener("input", onPasswordInput);
 {
      const res = await fetch("https://localhost:7113/Home/LogIn?login=" + login.value + "&password=" + password.value);
      const data = await res.json();
-     alert (data);
+     if (data == false) {
+         login.parentNode.setAttribute('data-validate', "Неправильний логін або пароль");
+         login.parentNode.classList.add('alert-validate');
+         password.parentNode.setAttribute('data-validate', "Неправильний логін або пароль");
+         password.parentNode.classList.add('alert-validate');
+     }
 }
