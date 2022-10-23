cd262dee18a33f835a170ad14afb1c89d7e45427
DF3203 <slvstgl90238@gmail.com>
2022-10-23 16:54:33 +0300

    Added redirect

---
+++ CourseWork/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseWork.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
